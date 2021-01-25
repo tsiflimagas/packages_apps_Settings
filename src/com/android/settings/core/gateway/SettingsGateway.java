Project-Awaken/android_packages_apps_Settings

Settings: Export Network traffic settings activity

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -161,7 +161,8 @@ import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSetting
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.awaken.settings.AwakenSettings;
-import com.awaken.settings.fragments.OmniJawsSettings;
+import com.awaken.settings.OmniJawsSettings;
+import com.awaken.settings.Traffic;
 
 public class SettingsGateway {
 
@@ -312,7 +313,8 @@ public class SettingsGateway {
             InteractAcrossProfilesDetails.class.getName(),
             MediaControlsSettings.class.getName(),
             AwakenSettings.class.getName(),
-            OmniJawsSettings.class.getName()
+            OmniJawsSettings.class.getName(),
+            Traffic.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -354,5 +356,6 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             Settings.AwakenSettingsActivity.class.getName(),
             Settings.OmniJawsSettingsActivity.class.getName(),
+            Settings.TrafficActivity.class.getName(),
     };
 }

tsiflimagas/packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -161,6 +161,7 @@ import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSetting
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.hycon.settings.HyconHub;
+import com.hycon.settings.Traffic;
 
 public class SettingsGateway {
 
@@ -310,7 +311,8 @@ public class SettingsGateway {
             InteractAcrossProfilesSettings.class.getName(),
             InteractAcrossProfilesDetails.class.getName(),
             MediaControlsSettings.class.getName(),
-            HyconHub.class.getName()
+            HyconHub.class.getName(),
+            Traffic.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -351,5 +353,6 @@ public class SettingsGateway {
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
             Settings.HyconHubActivity.class.getName(),
+            Settings.TrafficActivity.class.getName(),
     };
 }