c359e0c08af85878a85586e7e248e535078ea481
Jackeagle <jackeagle102@gmail.com>
2021-01-25 10:49:29 +0000

    Settings: Switch to string overlay for maintainer string
    - Define following overlay in your device tree
        <string name="bliss_maintainer"></string>

--- src/com/android/settings/deviceinfo/hycon/HyconMaintainerPreferenceController.java
+++ src/com/android/settings/deviceinfo/hycon/HyconMaintainerPreferenceController.java
@@ -17,7 +17,7 @@
 package com.android.settings.deviceinfo.hycon;
 
 import android.content.Context;
-import android.os.SystemProperties;
+import android.content.res.Resources;
 import android.text.TextUtils;
 
 import androidx.preference.Preference;
@@ -28,7 +28,6 @@ import com.android.settings.core.BasePreferenceController;
 public class HyconMaintainerPreferenceController extends BasePreferenceController {
 
     private static final String TAG = "HyconMaintainerPreferenceController";
-    private static final String ROM_PROPERTY = "ro.hycon.maintainer";
 
     public HyconMaintainerPreferenceController(Context context, String key) {
         super(context, key);
@@ -39,8 +38,7 @@ public class HyconMaintainerPreferenceController extends BasePreferenceControlle
     }
 
     public CharSequence getSummary() {
-        String rom = SystemProperties.get(ROM_PROPERTY,
-                this.mContext.getString(R.string.device_info_default));
-        return rom;
+        String maintainer = mContext.getResources().getString(R.string.hycon_maintainer);
+        return maintainer;
     }
 }
