omnirom/android_packages_apps_Settings

Settings: we can always encrypt if we dont have a battery at all
Change-Id: I3481cc42be52e983db6eca680677ab2bb77d8b39

src/com/android/settings/security/CryptKeeperSettings.java
@@ -69,11 +69,12 @@ public class CryptKeeperSettings extends InstrumentedPreferenceFragment {
             if (action.equals(Intent.ACTION_BATTERY_CHANGED)) {
                 final int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
                 final int plugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0);
+                final boolean present = intent.getBoolean(BatteryManager.EXTRA_PRESENT, true);
                 final int invalidCharger = intent.getIntExtra(
                     BatteryManager.EXTRA_INVALID_CHARGER, 0);
 
-                final boolean levelOk = level >= MIN_BATTERY_LEVEL;
-                final boolean pluggedOk =
+                final boolean levelOk = !present || level >= MIN_BATTERY_LEVEL;
+                final boolean pluggedOk = !present ||
                     ((plugged & BatteryManager.BATTERY_PLUGGED_ANY) != 0) &&
                      invalidCharger == 0;
 

tsiflimagas/packages_apps_Settings

src/com/android/settings/security/CryptKeeperSettings.java
@@ -69,11 +69,12 @@ public class CryptKeeperSettings extends InstrumentedPreferenceFragment {
             if (action.equals(Intent.ACTION_BATTERY_CHANGED)) {
                 final int level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0);
                 final int plugged = intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0);
+                final boolean present = intent.getBooleanExtra(BatteryManager.EXTRA_PRESENT, true);
                 final int invalidCharger = intent.getIntExtra(
                     BatteryManager.EXTRA_INVALID_CHARGER, 0);
 
-                final boolean levelOk = level >= MIN_BATTERY_LEVEL;
-                final boolean pluggedOk =
+                final boolean levelOk = !present || level >= MIN_BATTERY_LEVEL;
+                final boolean pluggedOk = !present ||
                     ((plugged & BatteryManager.BATTERY_PLUGGED_ANY) != 0) &&
                      invalidCharger == 0;
 